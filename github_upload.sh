a5f9af471c29c686d763a990cb7f12d3d477f78c
„dj--alex” <dj--alex@ya.ru>
2023-08-30 06:33:11 +0300

    10660

--- github_upload.sh
+++ github_upload.sh
@@ -1,12 +1,13 @@
 git add .
-git commit -m cat build.ini
+git commit -a $(cat build.ini) 
+git commit -m $(cat build.ini)
 git remote set-url origin git@github.com:dj--alex/m2k 
 git config pull.rebase true
 git config user.email „dj--alex@ya.ru”
 git config user.name „dj--alex”
-git push -u origin master
 git pull
 git pull --rebase origin master
+git push -u origin master
 
 # predw zaloginsja tut  ssh -T git@github.com !!!!   fdb
 #git config –global user.email „dj--alex@ya.ru”
